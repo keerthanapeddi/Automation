576f24d35f6c97a2e47cec638608b58e017a08da
Keerthana <keerthanap2006@gmail.com>
2020-08-05 19:57:26 +0530

    First Push to repository

---
+++ DemoQA/src/main/java/DemoQA/DemoQA/App.java
@@ -0,0 +1,13 @@
+package DemoQA.DemoQA;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
