9954159db3534a52c96b7912e4126f6ea90affdb
Keerthana <keerthanap2006@gmail.com>
2020-08-05 20:37:34 +0530

    Added BaseClass and launch functionality

--- DemoQA/src/main/java/Utilities/BaseClass.java
+++ DemoQA/src/main/java/Utilities/BaseClass.java
@@ -1,5 +1,20 @@
 package Utilities;
 
-public class BaseClass {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
+public class BaseClass 
+{
+	public WebDriver driver;
+	
+	public void launchBrowser()
+	{
+		System.setProperty("webdriver.chrome.driver", "Drivers\\chromedriver.exe");
+		driver = new ChromeDriver();
+		
+		driver.get("https://demoqa.com/");
+		driver.manage().window().maximize();
+		
+	}
+	
 }
